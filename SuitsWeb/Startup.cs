0a2d501fd75736ac8494378f5b6fd17533a59934
mohamed salah elkholy <67386713+MoohamedSalah@users.noreply.github.com>
2020-09-14 23:56:13 +0200

    Add project files.

---
+++ SuitsWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuitsWeb.Startup))]
+namespace SuitsWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
